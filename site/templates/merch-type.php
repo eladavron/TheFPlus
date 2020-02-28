27e98c8088357b74a6560fad4059c7e0f3ca9d5e
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2020-02-28 12:48:28 -0600

    I'm probably abandoning this idea.

--- site/templates/merch-type.php
+++ site/templates/merch-type.php
@@ -4,6 +4,7 @@
   $pubdate = date('l, F jS Y', $page->date());
   $pubtime = date("g:ia", strtotime($page->time()));
   require_once('stripe-php/init.php');
+  \Stripe\Stripe::setApiKey('sk_test_5OebpkDdlb1mnEeq8HVM1i8a');
 ?>
 
 <main class="main page" role="main">
@@ -50,22 +51,17 @@
         <?php echo $page->text()->kirbytext() ?>
       </div>
 
-
-      <div class="stripe-box">
-        <h1>Hello!</h1>
+      <div class="stripe-test-block">
         <?php 
-          \Stripe\Stripe::setApiKey('sk_test_5OebpkDdlb1mnEeq8HVM1i8a');
           $intent = \Stripe\PaymentIntent::create([
             'amount' => 1099,
             'currency' => 'usd',
           ]);
-          $output = [
-            'publishableKey' => 'pk_test_7fx1utWsqpOCrlIqTycFTSi7',
-            'clientSecret' => $intent->client_secret,
-          ];
-
-          echo '<script> const data = { publishableKey: "pk_test_7fx1utWsqpOCrlIqTycFTSi7", clientSecret: '. $intent->client_secret .' }</script>';
         ?>
+      
+        <script>
+          console.table(`<?php var_dump($intent); ?>`);
+        </script>
 
         <form id="payment-form">
           <div id="card-element">
@@ -75,106 +71,54 @@
           <!-- We'll put the error messages in this element -->
           <div id="card-errors" role="alert"></div>
 
-          <button style="font-size:60px;" id="submit">Pay</button>
+          <button id="submit">Pay</button>
         </form>
 
         <script src="https://js.stripe.com/v3/"></script>
         <script>
+          var stripe = Stripe('pk_test_7fx1utWsqpOCrlIqTycFTSi7');
+          var elements = stripe.elements();
           
+          var style = {
+            base: {
+              color: "#32325d",
+            }
+          };
 
-          // A reference to Stripe.js
-var stripe;
+          var card = elements.create("card", { style: style });
 
-var orderData = {
-  items: [{ id: "photo-subscription" }],
-  currency: "usd"
-};
+          $(document).ready(function() {
+            card.mount("#card-element");
 
-// Disable the button until we have Stripe set up on the page
-//document.querySelector("button").disabled = true;
-
-stripe = Stripe(data.publishableKey);
-var elements = stripe.elements();
-var card = elements.create("card", { style: style });
-card.mount("#card-element");
-const clientSecret = data.clientSecret;
-
-// Handle form submission.
-var form = document.getElementById("payment-form");
-form.addEventListener("submit", function(event) {
-  event.preventDefault();
-  // Initiate payment when the submit button is clicked
-  pay(stripe, card, clientSecret);
-});
-
-/*
- * Calls stripe.confirmCardPayment which creates a pop-up modal to
- * prompt the user to enter extra authentication details without leaving your page
- */
-var pay = function(stripe, card, clientSecret) {
-  changeLoadingState(true);
-
-  // Initiate the payment.
-  // If authentication is required, confirmCardPayment will automatically display a modal
-  stripe
-    .confirmCardPayment(clientSecret, {
-      payment_method: {
-        card: card
-      }
-    })
-    .then(function(result) {
-      if (result.error) {
-        // Show error to your customer
-        showError(result.error.message);
-      } else {
-        // The payment has been processed!
-        orderComplete(clientSecret);
-      }
-    });
-};
-
-/* ------- Post-payment helpers ------- */
-
-/* Shows a success / error message when the payment is complete */
-var orderComplete = function(clientSecret) {
-  // Just for the purpose of the sample, show the PaymentIntent response object
-  stripe.retrievePaymentIntent(clientSecret).then(function(result) {
-    var paymentIntent = result.paymentIntent;
-    var paymentIntentJson = JSON.stringify(paymentIntent, null, 2);
-
-    document.querySelector(".sr-payment-form").classList.add("hidden");
-    document.querySelector("pre").textContent = paymentIntentJson;
-
-    document.querySelector(".sr-result").classList.remove("hidden");
-    setTimeout(function() {
-      document.querySelector(".sr-result").classList.add("expand");
-    }, 200);
-
-    changeLoadingState(false);
-  });
-};
-
-var showError = function(errorMsgText) {
-  changeLoadingState(false);
-  var errorMsg = document.querySelector(".sr-field-error");
-  errorMsg.textContent = errorMsgText;
-  setTimeout(function() {
-    errorMsg.textContent = "";
-  }, 4000);
-};
-
-// Show a spinner on payment submission
-var changeLoadingState = function(isLoading) {
-  if (isLoading) {
-    document.querySelector("button").disabled = true;
-    document.querySelector("#spinner").classList.remove("hidden");
-    document.querySelector("#button-text").classList.add("hidden");
-  } else {
-    document.querySelector("button").disabled = false;
-    document.querySelector("#spinner").classList.add("hidden");
-    document.querySelector("#button-text").classList.remove("hidden");
-  }
-};
+            var form = document.getElementById('payment-form');
+            
+            form.addEventListener('submit', function(ev) {
+              ev.preventDefault();
+              stripe.confirmCardPayment('<?= $intent['client_secret']; ?>', {
+                payment_method: {
+                  card: card,
+                  billing_details: {
+                    name: 'Jenny Rosen'
+                  }
+                }
+              }).then(function(result) {
+                if (result.error) {
+                  // Show error to your customer (e.g., insufficient funds)
+                  console.log(result.error.message);
+                } else {
+                  // The payment has been processed!
+                  if (result.paymentIntent.status === 'succeeded') {
+                    console.log(result);
+                    // Show a success message to your customer
+                    // There's a risk of the customer closing the window before callback
+                    // execution. Set up a webhook or plugin to listen for the
+                    // payment_intent.succeeded event that handles any business critical
+                    // post-payment actions.
+                  }
+                }
+              });
+            });
+          });
 
         </script>
 
